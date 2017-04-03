0b4b861b7d19b220e674831f87516c18ae60aa02
jl10812 <jl10812@ic.ac.uk>
2017-04-03 22:40:43 +0100

    commented bash script

--- runme.sh
+++ runme.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 #PBS -l walltime=03:00:00,mem=1gb
-cp $HOME/1mR_finite.py $WORK 
+
+## select Python script to work on, copy it and move to work directory:
+
+cp $HOME/1mR_finite.py $WORK ## modify script to run here
 cd $WORK
+
+## set up and load python and interpreter and run script:
+
 module load anaconda3/4.3.1
-python 1mR_finite.py
+python 1mR_finite.py ## modify script to run here
+
+## copy outputted graphs back to home directory and erase from work directory
 cp *.png $HOME
 rm *.png
 
