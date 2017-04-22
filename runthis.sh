3eaa3cd7bdc24878ac544a7c9cfd9254ca842525
jl10812 <jl10812@ic.ac.uk>
2017-04-22 02:49:53 +0100

    changed default model to run to all_multi

--- runthis.sh
+++ runthis.sh
@@ -2,12 +2,12 @@
 #PBS -l walltime=72:00:00,mem=100gb
 
 ## select Python script to run, copy it and move to work directory:
-cp $HOME/1mR_finite.py $WORK ## select script to run here
+cp $HOME/all_multi.py $WORK ## select script to run here
 cd $WORK
 
 ## load Anaconda Python and run script using Python
 module load anaconda3/4.3.1
-python 1mR_finite.py ## select script to run here
+python all_multi.py ## select script to run here
 
 ## copy outputted graphs back to home directory and erase from work directory
 cp *.png $HOME
