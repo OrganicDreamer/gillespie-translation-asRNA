e3300b28857f72e2b342177f5af56ea4d94b54ba
jl10812 <jl10812@ic.ac.uk>
2017-06-06 13:12:29 +0100

    fixed sweep range creation

--- 1mR_finite_sweep.py
+++ 1mR_finite_sweep.py
@@ -34,7 +34,6 @@ sweep_kinetic_const = np.array(
 # Translation initiation, elongation, completion rates
 start_transl = 0.0833333333333333
 transl_rate = 15
-fin_transl = range(transl_rate/500,(50*transl_rate)+transl_rate/10,transl_rate/10)
 
 # Array of values for translation elongation rates of specific codons on mRNA, not including stop codon or RBS:
 codon_transl_rates = np.full((length_mrna - 2), transl_rate)
